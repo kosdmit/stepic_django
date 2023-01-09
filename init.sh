4e449e3466531807b3f871fe2e4f915b86c4cb0e
kosdmit <kosdmit@hotmail.com>
2023-01-09 15:32:42 +0400

    fix from another GIT repository

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
-sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx stop
+sudo /etc/init.d/nginx start
