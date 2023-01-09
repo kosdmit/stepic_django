5b4f78a63aaea02863af02b1368b7f9c2fabc0da
kosdmit <kosdmit@hotmail.com>
2023-01-09 19:00:16 +0400

    step 2 from another GIT repository

--- init.sh
+++ init.sh
@@ -1,4 +1,16 @@
+#####init_nginx#####
+# include our settings to nginx main config
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
+# delete default location settings
 sudo rm -rf /etc/nginx/sites-enabled/default
+# start/restart nginx
 sudo /etc/init.d/nginx stop
 sudo /etc/init.d/nginx start
+
+#####init_gunicorn#####
+# delete example settings
+sudo rm /etc/gunicorn.d/*
+# include setting for hello.py to gunicorn
+sudo ln -sf /home/box/web/etc/hello.conf /etc/gunicorn.d/hello.conf
+# start gunicorn
+sudo /etc/init.d/gunicorn start
